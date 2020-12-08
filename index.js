d32d03138fa60cfaae07b034c18dd37a4beb0013
unknown <canparekh@gmail.com>
2020-12-07 19:59:16 -0500

    setup

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-var server = http.createServer(app);
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-    res.send("Hi There Bitch!");
-})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-server.listen(3000, function() {
-    console.log('Listening on port 3000...');
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
