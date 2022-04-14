77aefa9f9f81b1c9fec23969dceb43c4396f7697
Jaan <canparekh@gmail.com>
2022-04-13 22:14:08 -0400

    Database connection

--- chatroom.js
+++ chatroom.js
@@ -6,6 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const path = require("path");
 const { isUndefined } = require("util");
+const mysql = require('mysql');
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,8 +14,39 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/chatroom.html");
 });
 
+app.get("/hello", (req, res) => {
+  res.sendFile(__dirname + "/public/chatroom.html");
+});
+
+app.get("/chatroom", (req, res) => {
+  res.sendFile(__dirname + "/public/chatroom.html");
+});
+
+const database_connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'wassup',
+  password: 'hello123',
+  database: 'chatroom'
+});
+
+database_connection.connect(function(e) {
+  if (e) throw e;
+  console.log("Database Connected!");
+});
+
+function saveMessage(sql){
+  database_connection.query(sql, function (e, res){
+  if (e) throw e;  
+  });
+}
+
+io.of("/hello").on("connection", (socket) => {
+  console.log("hello world1!!1");
+});
+
+
 //when a user establishes a new connection
-io.on("connection", (socket) => {
+io.of("/").on("connection", (socket) => {
   socket.on("nickname", (nickname) => {
     if (!nickname.trim() || !nickname) {
       nickname = "Anonymous";
@@ -26,8 +58,17 @@ io.on("connection", (socket) => {
 
     socket.username = nickname;
 
-    io.emit("chat message", socket.username + " has entered the chat.");
+    database_connection.query("SELECT * FROM messages WHERE chatroom_id = 1 and is_private = false order by message_id desc limit 50", function (e, res){
+      if (e) throw e;
+      io.emit("load previous messages", res);
+      io.emit("chat message", socket.username + " has entered the chat.");
+    });
+
+    
     checkConnectedUsers();
+
+    
+
   });
 
   //when the user disconnects
@@ -61,8 +102,10 @@ io.on("connection", (socket) => {
   }
 
   //when the user sends a chat message
-  socket.on("chat message", (msg) => {
-    socket.broadcast.emit("chat message", socket.username + ": " + msg);
+  socket.on("chat message", (message) => {
+    socket.broadcast.emit("chat message", socket.username + ": " + message);
+    let sql_query = "INSERT INTO messages (chatroom_id, sent_by, message, is_private) VALUES(1," + mysql.escape(socket.username) + "," + mysql.escape(message) + ",false)";
+    saveMessage(sql_query);
   });
 
   // private messaging
@@ -75,6 +118,9 @@ io.on("connection", (socket) => {
       }
     }
     socket.to(id).emit("private message", socket.username, message);
+    let sql_query = "INSERT INTO messages (chatroom_id, sent_by, message, is_private, sent_to) VALUES(1," + mysql.escape(socket.username) + "," + mysql.escape(message)
+     + ",true, " + mysql.escape(sendTo) + ")";
+     saveMessage(sql_query);
   });
 
   //when the user is typing
@@ -88,4 +134,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(80, () => {});
+server.listen(3000, () => {});
